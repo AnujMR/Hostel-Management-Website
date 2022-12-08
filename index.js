a46fd51a310c234444bb1744763c5cf958d98a61
AnujMR <anujramane22@gmail.com>
2022-12-08 15:27:41 +0530

    bootstrap installed and integrated

--- index.js
+++ index.js
@@ -7,31 +7,32 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static('public'));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Server started at port 3000");
 });
 
 app.get("/", async (req, res) => {
-    res.sendFile(__dirname + "/front_end/html/dashboard.html");
+    res.sendFile(__dirname + "/public/html/dashboard.html");
 });
 
 app.get("/dashboard.html", async (req, res) => {
-    res.sendFile(__dirname + "/front_end/html/dashboard.html");
+    res.sendFile(__dirname + "/public/html/dashboard.html");
 });
 
 app.get("/profile.html", async (req, res) => {
-    res.sendFile(__dirname + "/front_end/html/profile.html");
+    res.sendFile(__dirname + "/public/html/profile.html");
 });
 
 app.get("/rooms.html", async (req, res) => {
-    res.sendFile(__dirname + "/front_end/html/rooms.html");
+    res.sendFile(__dirname + "/public/html/rooms.html");
 });
 
 app.get("/students.html", async (req, res) => {
-    res.sendFile(__dirname + "/front_end/html/students.html");
+    res.sendFile(__dirname + "/public/html/students.html");
 });
 
 app.get("/staff.html", async (req, res) => {
-    res.sendFile(__dirname + "/front_end/html/staff.html");
+    res.sendFile(__dirname + "/public/html/staff.html");
 });
