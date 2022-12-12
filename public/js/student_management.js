89a5c970d45751543df380f14cecc01c706c80c2
AnujMR <anujramane22@gmail.com>
2022-12-12 19:04:50 +0530

    Student details screen created, expenses graph added

--- public/js/student_management.js
+++ public/js/student_management.js
@@ -1,3 +1,15 @@
 $(".view-details-btn").on("click", function () {
     location.assign("/public/html/studentDetails.html");
 });
+
+export function searchStudent(queryText, studentList) {
+    var i = 0;
+    var searchResults = [];
+    for (i = 0; i < studentList.length; i++) {
+        if (studentList[i]["name"].search(queryText) != -1) {
+            searchResults.add(studentList[i]);
+        }
+    }
+
+    return searchResults;
+}
